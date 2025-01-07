8fd794005c30cc168c44bbd1b913a96ca545e31b
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-01-08 01:35:57 +0530

    init commit

---
+++ 1_log_comment.js
@@ -0,0 +1,7 @@
+console.log('Hello, world!');
+
+//Single line Comment
+
+/*
+Multi line comment
+*/
---
+++ 2_variable.js
@@ -0,0 +1,23 @@
+var a=1
+console.log(a)
+console.log(typeof a) 
+
+const b=1.5
+console.log(b) 
+console.log(typeof b) 
+
+let c=a+b
+console.log(c) 
+console.log(typeof c) 
+
+var d='Hello '
+console.log(d)
+console.log(typeof d)
+
+const e='world!'
+console.log(e)
+console.log(typeof e)
+
+let f=d+e
+console.log(f)
+console.log(typeof f)
