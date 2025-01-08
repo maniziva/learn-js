d7acb3f23c352ae3aefbc703632a7c432d0d49ed
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-01-08 14:04:45 +0530

    update: If Condition

---
+++ 3_condition.js
@@ -0,0 +1,14 @@
+const flag=true
+if(flag){
+    console.log('True')}
+    else{
+        console.log('False')
+    }
+
+
+const flag2=false
+if(flag2){
+    console.log('True')}
+    else{
+        console.log('False')
+    }
