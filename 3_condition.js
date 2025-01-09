a950d45def967006dceb2fade68889b77893fbf0
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-01-09 12:02:27 +0530

    update: do_while

--- 3_condition.js
+++ 3_condition.js
@@ -10,9 +10,20 @@ if(!flag){
     else{
         console.log('False')
     }
-    
+
 let i=0
 while(i<10){
     console.log(i)
     i++
+}
+
+
+let j=0
+do{
+    j++
+    console.log(j)
+}
+while(j<10)
+{
+    console.log('do while loop'+j)
 }
