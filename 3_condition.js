8ba1e75d10e839ed4cd1520cb6e054759db7e111
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-01-09 12:15:42 +0530

    update: condition with for loop

--- 3_condition.js
+++ 3_condition.js
@@ -4,20 +4,19 @@ if(flag){
     else{
         console.log('False')
     }
-
+console.log('-------------------')
 if(!flag){
     console.log('True')}
     else{
         console.log('False')
     }
-
+console.log('-------------------')
 let i=0
 while(i<10){
     console.log(i)
     i++
 }
-
-
+console.log('-------------------')
 let j=0
 do{
     j++
@@ -27,8 +26,10 @@ while(j<10)
 {
     console.log('do while loop'+j)
 }
-
-
-for(let k=0;k<10;k++){
-    console.log(k)
+console.log('-------------------')
+for(let k=0; k<100; k++){
+    if(k%2 == 0 && k%5==0){ // Even Numbers along with multiple of 5
+        console.log(k); 
+    }
 }
+console.log('-------------------')
