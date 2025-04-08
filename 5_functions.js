03d1da72a2627da0a49f63e61865e7e1d87c341e
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-04-08 23:58:45 +0530

    update: function

--- 5_functions.js
+++ 5_functions.js
@@ -4,3 +4,8 @@ function add(a, b) {
 console.log("Basic Function:",add(2, 3)); // Logs 5
 console.log('-----------------');
 
+let add2 = function(a, b) {
+    return a + b;
+}
+console.log("Anonymous Function:",add2(2, 3)); // Logs 5
+console.log('-----------------');
