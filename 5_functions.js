c0fb7f5ffa92768bf251a231ce6322a04500bf9d
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-04-10 23:52:50 +0530

    update: arrow function2

--- 5_functions.js
+++ 5_functions.js
@@ -15,3 +15,8 @@ let add3 = (a, b) => {
 }
 console.log("Arrow Function:",add3(2, 3)); // Logs 5
 console.log('-----------------');
+
+let add4 = (a, b) => a + b;
+console.log("Arrow Function:",add4(2, 3)); // Logs 5
+console.log('-----------------');
+
