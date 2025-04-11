641df0330cb63793b17ec88a257cba991a9e3984
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-04-11 10:56:38 +0530

    update: slice and substring

--- 6_string.js
+++ 6_string.js
@@ -4,3 +4,18 @@ console.log(day.toUpperCase()); // Logs 'FRIDAY'
 console.log(day.toLowerCase()); // Logs 'friday'    
 console.log(day.length); // Logs 6
 console.log(day.charAt(0)); // Logs 'f'
+console.log(day.charAt(day.length - 1));// Logs 'y'
+
+console.log(day.indexOf('F')); // Logs 0
+console.log(day[1]); //r
+
+const getlastindex = day.length - 1;
+const lastchar = day[getlastindex];
+console.log(lastchar);
+
+
+const last2char = day.slice(-2);
+console.log(last2char); // Logs 'ay'
+
+let firstTwo = day.substring(0, 2);
+console.log(firstTwo); // Logs 'Fr'
