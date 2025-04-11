46d76d56aad8f7fb245cdf8afef0f5afe6ccb148
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-04-11 10:35:23 +0530

    Add: String related scripts

---
+++ 6_string.js
@@ -0,0 +1,6 @@
+let day = 'Friday';
+console.log(day); // Logs 'friday'
+console.log(day.toUpperCase()); // Logs 'FRIDAY'
+console.log(day.toLowerCase()); // Logs 'friday'    
+console.log(day.length); // Logs 6
+console.log(day.charAt(0)); // Logs 'f'
